60de084f7dd0b72dfa515f6d8099ea203d9ede3c
s22002 <s22002@std.it-college.ac.jp>
2023-02-23 11:38:28 +0000

    ゲームの勝敗の判定

--- pages/tictactoe.js
+++ pages/tictactoe.js
@@ -13,6 +13,9 @@ const Board = props => {
 
   const handleClick = (i) => {
     const squares = state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = state.xIsNext ? 'X' : 'O'
     setState({ squares, xIsNext: !state.xIsNext })
   }
@@ -25,7 +28,13 @@ const Board = props => {
       />
     )
   }
-  const status = 'Next player: ' + (state.xIsNext ? 'X' : 'O')
+  const winner = calculateWinner(state.squares)
+  let status
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + (state.xIsNext ? 'X' : 'O')
+  }
   return (
     <div>
       <div className='status'>{status}</div>
@@ -61,5 +70,23 @@ const Game = prpos => {
     </div>
   )
 }
-
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
 export default Game
